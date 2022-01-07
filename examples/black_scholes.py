d8a728d21c4c3a393dc55e47d9105b4a20ec2e4c
Nikolai Nowaczyk <mail@nikno.de>
2022-01-07 17:48:45 +0000

    deleted spurious folder

--- examples/black_scholes.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append(r"D:\clouds\github\quantfinance-examples-dev")
-sys.path.append(r"D:\clouds\github\quantfinance-examples-dev\pyqfin")
-from pyqfin.models.black_scholes import Parameters, Analytic
-
-params = Parameters(sigma=0.2, r=0.03)
-a = Analytic(params)
-
